c92760ce4000a1fa79a8344293ee5afcd41119ed
Yew Yen Sing <yewyensing@gmail.com>
2021-08-16 10:42:58 +0800

    YS latest code
    Updated Notification and Tab3 page.

--- src/app/notification/notification.page.ts
+++ src/app/notification/notification.page.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { Plugins } from '@capacitor/core';
+import { ModalController } from '@ionic/angular';
+import { UserProfilePage } from '../user-profile/user-profile.page';
+
 
 const { Storage } = Plugins;
 @Component({
@@ -13,6 +16,7 @@ const { Storage } = Plugins;
 export class NotificationPage implements OnInit {
 
   requests: any = [];
+  notifications: any =[];
   submitted: boolean = false;
   USERID: string = '2';
 
@@ -26,46 +30,58 @@ export class NotificationPage implements OnInit {
   buddyid1: string;
   buddyid2: string;
 
-  constructor(private route: ActivatedRoute, public http: HttpClient, private router: Router,  private toastController: ToastController) {
+  today: String = new Date().toISOString();
+  year: any = new Date().getFullYear();
+
+  displayDate: String;
+
+  constructor(private route: ActivatedRoute, public http: HttpClient, private router: Router,  private toastController: ToastController, public modalController: ModalController) {
     this.id = this.route.snapshot.params.id;
     
-    console.log('notiUserid' + this.id)
+    console.log('Userid' + this.id)
    }
 
 
   ngOnInit() {
-    
-    
-    var url = 'https://buddyfind.herokuapp.com/notification/'+this.id+'';
+    this.getRequests();
+    this.getNotification();
+    console.log(this.today)
+  }
+  async getRequests(){
+    var url = 'https://itj-findabuddy.herokuapp.com/notification/'+this.id+'';
     this.http.get(url).subscribe(data => {
       this.requests = data
-      console.log('notidata:' +data)
+      console.log(data)
     })
 
   }
-
-
+  async getNotification(){
+    var urlNotification = 'https://itj-findabuddy.herokuapp.com/requestResult';
+    this.http.get(urlNotification).subscribe(notiData => {
+      this.notifications = notiData
+      console.log(notiData)
+      
+    })
+  }
 
   accept(){
-    var url = 'https://buddyfind.herokuapp.com/update';
+    var url = 'https://itj-findabuddy.herokuapp.com/update';
     var status = "Accepted";
     for (let request of this.requests){
       this.requestId = request.idRequest 
     }
-    var postData = JSON.stringify({
-     requestId: this.requestId,
-     status: "Accepted"
-     
-    });
+
 
     var insertBuddy  = JSON.stringify({
+      requestId: this.requestId,
       userid1: this.id,
       buddyid1: document.getElementById("label2").textContent,
       userid2: document.getElementById("label2").textContent,
       buddyid2: this.id,
+      today: this.today,
+      status: "Accepted",
     })
 
-    console.log('test: ' + postData)
     console.log("insert: " + insertBuddy)
 
     const httpOptions = {
@@ -75,23 +91,6 @@ export class NotificationPage implements OnInit {
         'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE'
       })
     };
-    this.http.post(url, postData, httpOptions).subscribe((data) => {
-      console.log('postData:', postData)
-      console.log('data', data);
-      console.log(this.requestId)
-      if (data != null)
-      {
-        window.location.reload();
-        console.log(data);
-
-      }
-      else{
-        console.log(data);
-
-        }
-      }, error => {
-          console.log(error);
-      });
 
       this.http.post(url, insertBuddy, httpOptions).subscribe((data) => {
         console.log('postData:', insertBuddy)
@@ -110,20 +109,25 @@ export class NotificationPage implements OnInit {
         }, error => {
             console.log(error);
         });
+     
+    //  window.location.reload();
 
       this.requestAccepted();
-  //    this.router.navigate(['notification/'+this.USERID+'']); //Navigate back after updating the db
+    
+    // this.router.navigate(['notification/'+this.USERID+'']); //Navigate back after updating the db
 
+    // window.location.reload();
     }
 
     reject(){
-      var url = 'https://buddyfind.herokuapp.com/update';
+      var url = 'https://itj-findabuddy.herokuapp.com/update';
       for (let request of this.requests){
         this.requestId = request.idRequest 
       }
       var postData = JSON.stringify({
        requestId: this.requestId,
-       status: "Rejected"
+       status: "Rejected",
+       today: this.today
        
       });
       console.log('test: ' + postData)
@@ -168,12 +172,12 @@ export class NotificationPage implements OnInit {
 
 
 
-      async loginUser() {
-        const { value } = await Storage.get({ key: 'userID' });
-        console.log('Got item: ', value);
-        this.id = value;
-        console.log(this.id)
-      }
+    async loginUser() {
+      const { value } = await Storage.get({ key: 'userID' });
+      console.log('Got item: ', value);
+      this.id = value;
+      console.log(this.id)
+    }
 
   
   async requestAccepted() {
@@ -185,17 +189,68 @@ export class NotificationPage implements OnInit {
     color: 'secondary'
     });
     toast.present();
+    return true;
     }
 
-    async requestRejected() {
-      var requester = document.getElementById("username").textContent;
-      const toast = await this.toastController.create({
-      message: 'You have rejected ' + requester + ' buddy request.',
-      duration: 2000,
-      position: 'top',
-      color: 'secondary'
-      });
-      toast.present();
-      }
+  async requestRejected() {
+    var requester = document.getElementById("username").textContent;
+    const toast = await this.toastController.create({
+    message: 'You have rejected ' + requester + ' buddy request.',
+    duration: 2000,
+    position: 'top',
+    color: 'secondary'
+    });
+    toast.present();
+    }
+    
 
+    async displayProfile(list){
+
+      const modal = await this.modalController.create({
+        
+        component: UserProfilePage,
+        
+        componentProps: {
+          'username': list.username,
+          'gender': list.Gender,
+          'fitnessLevel': list.fitnessLevel,
+          'image': list.image,
+          'age': this.year - Number(list.DOB.slice(-4)),
+        },
+        cssClass: 'my-custom-modal-css',
+        
+
+      })
+      console.log(list)
+      return await modal.present();
+    
+    }
+
+    delete(noti){
+      var url = 'https://itj-findabuddy.herokuapp.com/deleteNoti';
+      var deletedata = JSON.stringify({
+      requestId : noti.idRequest
+      });
+      console.log(noti)
+      const httpOptions = {
+      headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE'
+      })
+      };
+      this.http.post(url, deletedata, httpOptions).subscribe((data) => {
+      console.log('deletedata:', deletedata)
+      console.log(data);
+      if (data != null) {
+        window.location.reload();
+
+      // this.failed()
+      } 
+      }, error => {
+      console.log(error);
+      });
+
+      }
+      
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -4,7 +4,11 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { Plugins } from '@capacitor/core';
-import { ToastController } from '@ionic/angular';
+import { IonSearchbar, AlertController, ToastController } from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+import { ViewChild } from '@angular/core';
+
 
 const { Storage } = Plugins;
 
@@ -16,6 +20,11 @@ const { Storage } = Plugins;
 export class Tab3Page {
 
   user: any = [];
+  check: any = [];
+
+  @ViewChild('searchBar', {static: false}) searchBar: IonSearchbar;
+
+
   submitted: boolean = false;
   send: boolean = true;
   searchBuddy: FormGroup;
@@ -27,41 +36,135 @@ export class Tab3Page {
   imageUrl = null;
  photo: Blob;
 
+ latitude: any = 0; //latitude
+ longitude: any = 0; //longitude
+ address: string = ''
+currentLocation: string = '' 
+ today: String = new Date().toISOString();
 
-  constructor(private route: ActivatedRoute, public http: HttpClient, private router: Router, private toastController: ToastController) {
+
+ year: any = new Date().getFullYear();
+age: any;
+checkAge: boolean = false;
+userAge: any =[]
+
+  constructor(private route: ActivatedRoute, public http: HttpClient, private router: Router, private toastController: ToastController, private alertCtrl: AlertController, private geolocation: Geolocation,     private nativeGeocoder: NativeGeocoder
+    ) {
 
     this.searchBuddy = new FormGroup({
-      age: new FormControl(),
-      gender: new FormControl(''),
-      location: new FormControl('', [Validators.required]),
+      
+      location: new FormControl('Select a location', [Validators.required]),
     })
     
   }
 
+  isItemAvailable: boolean = false;
+  items = [];
+  current = [];
+
+  initializeItems(){
+    
+      this.items = ["Aljunied", "Ang Mo Kio", "Bedok", "Bishan", "Boon Lay", "Bukit Batok",
+      "Bukit Merah", "Bukit Panjang", "Bukit Timah", "Changi", "Clementi",
+      "East Coast", "Hougang", "Jurong","Marina Bay", "Sembawang", "Tampines", "Toa Payoh",
+    "Woodlands"];
+  }
+
+  getItems(ev: any) {
+      // Reset items back to all of the items
+      this.initializeItems();
+
+      // set val to the value of the searchbar
+      const val = ev.target.value;
+
+      // if the value is an empty string don't filter the items
+      if (val && val.trim() !== '') {
+          this.isItemAvailable = true;
+          this.items = this.items.filter((item) => {
+              return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+          })
+      } else {
+        
+          this.isItemAvailable = false;
+      }
+  }
+
   async loginUser() {
     const { value } = await Storage.get({ key: 'userID' });
-    console.log('tab3userid: ', value);
+    console.log('Got item: ', value);
     this.id = value;
-  }
+    console.log(this.id)
+          var url = 'https://itj-findabuddy.herokuapp.com/randomUsers';
 
-  ngOnInit(){
-    this.loginUser()
-
-  }
-
-  search(){
-    this.submitted =true;
-    var url = 'https://buddyfind.herokuapp.com/getUser';
 
     var postData = JSON.stringify({
       currentUser: this.id,
 
-      location: this.searchBuddy.value['location']
+     });
+ 
+  
+     console.log(postData)
+  
+     const httpOptions = {
+       headers: new HttpHeaders({
+         'Content-Type': 'application/json',
+         'Access-Control-Allow-Origin': '*',
+         'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE'
+       })
+     };
+ 
+     this.http.post(url, postData, httpOptions).subscribe((data) => {
+       console.log('postData:', postData)
+       
+       this.user = data;
+       for (let i = 0; i < this.user.length; i++) {
+        this.age = this.year - Number(data[i]["DOB"].slice(-4))
+        data[i]["DOB"] =this.age
+      }
+
+       if (data != null){
+         console.log(data) //error msg
+        this.checkAge = true;
+        console.log(this.checkAge)
+        
+       }
+
+      else{
+       }
+       }, error => {
+           console.log(error);
+       });
+ 
+  }
+
+  ngOnInit(){
+    this.loginUser()
+    
+    this.getCurrentLocation();
+  }
+
+  search(item){
+    console.log(item)
+//    this.submitted =true;
+
+
+    var url = 'https://itj-findabuddy.herokuapp.com/getUser';
+
+    if (item === "current"){
+      item = this.currentLocation
+      console.log(item)
+    }
+    
+
+    var postData = JSON.stringify({
+      currentUser: this.id,
+
+      location: item
       //front end value
     });
 
- 
-    console.log(postData)
+  
+    console.log(postData[2])
  
     const httpOptions = {
       headers: new HttpHeaders({
@@ -74,30 +177,60 @@ export class Tab3Page {
     this.http.post(url, postData, httpOptions).subscribe((data) => {
       console.log('postData:', postData)
       console.log('data', data);
-      this.user = data
 
+      this.user = data
+      for (let i = 0; i < this.user.length; i++) {
+       this.age = this.year - Number(data[i]["DOB"].slice(-4))
+       data[i]["DOB"] =this.age
+       
+       console.log(data[i]["DOB"])
+       console.log(data)
+      }
       if (this.user.length != 0){
+        
+    
+     
         console.log("pass")
-      }else{
+      }
+      if (data != null){
+        console.log(data) //error msg
+        this.age = this.year - Number(this.age.slice(-4));
+       console.log(this.age)
+       this.checkAge = true;
+       console.log(this.checkAge)
+       
+      }
+      else{
         this.noUserFound();
       }
+      }
       
-      }, error => {
+      , error => {
           console.log(error);
-      });
-
+      })
 
   }
+  
 
   
 
-  sendRequest(){
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+    subHeader: 'Request Sent!',
+    message: 'You have already sent a request to the user.',
+    buttons: ['Dismiss']
+   });
+   await alert.present(); 
+}
+  
+   sendRequest(list){
     this.submitted =true;
-    var url = 'https://buddyfind.herokuapp.com/sendRequest';
-
+    console.log(list.userID)
+    var url = 'https://itj-findabuddy.herokuapp.com/sendRequest';
     var postData = JSON.stringify({
      currentUser: this.id,
-     receiverId: document.getElementById('label1').textContent
+     receiverId: list.userID,
+     today: this.today
 
      //front end value
     });
@@ -116,7 +249,14 @@ export class Tab3Page {
     this.http.post(url, postData, httpOptions).subscribe((data) => {
       console.log('postData:', postData)
       console.log('data', data);
-      
+      this.check = data;
+      console.log(this.check);
+      if (this.check.length == 1){
+        this.presentAlert();
+        console.log("request has been sent.") //error msg
+      }else{
+        this.requestSent();
+      }
       }, error => {
           console.log(error);
       });
@@ -136,6 +276,88 @@ export class Tab3Page {
       });
       toast.present();
       }
+
+      getCurrentLocation(){
+        this.geolocation.getCurrentPosition().then((resp) => {
+          // resp.coords.latitude
+          // resp.coords.longitude
+          this.latitude = resp.coords.latitude;
+          this.longitude = resp.coords.longitude;
+          console.log(this.latitude)
+         }).catch((error) => {
+           console.log('Error getting location', error);
+         });
+         
+         let watch = this.geolocation.watchPosition();
+         watch.subscribe((data) => {
+          // data can be a set of coordinates, or an error (if an error occurred).
+      
+      
+          var url = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat='+this.latitude+'&lon='+this.longitude
+          console.log(url)
+          this.http.get(url).subscribe(data => {
+           // this.requests = data
+           this.currentLocation = data["address"]["suburb"]
+            console.log(data["address"]["suburb"])
+          })
+    
+         // return this.currentLocation
+         });
+      } 
+/* 
+      async randomUsers(){
+        var url = 'https://itj-findabuddy.herokuapp.com/randomUsers';
+        this.http.get(url).subscribe(data => {
+          this.user = data
+          console.log(data)
+        })
+      } */
+
+    async randomUsers(){
+      var url = 'https://itj-findabuddy.herokuapp.com/randomUsers';
+      var postData = JSON.stringify({
+        currentUser: this.id,
+
+       });
+   
+    
+       console.log(postData)
+    
+       const httpOptions = {
+         headers: new HttpHeaders({
+           'Content-Type': 'application/json',
+           'Access-Control-Allow-Origin': '*',
+           'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE'
+         })
+       };
+   
+       this.http.post(url, postData, httpOptions).subscribe((data) => {
+         console.log('postData:', postData)
+         console.log('data', data);
+         this.user = data;
+         this.age = data[0]["DOB"]
+
+         console.log(this.check);
+         if (data != null){
+           console.log(data) //error msg
+         }
+        
+        else{
+         }
+         }, error => {
+             console.log(error);
+         });
+   
+    }
+async requestSent() {
+  const alert = await this.alertCtrl.create({
+  subHeader: 'Request Sent!',
+  message: 'Buddy request has been sent.',
+  buttons: ['Dismiss']
+ });
+ await alert.present(); 
+}
+
 
 
   }
