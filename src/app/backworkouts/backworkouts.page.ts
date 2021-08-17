7cb71b2b24fcb5cbedd7ee3113dd40d7f067b290
shawn <21a672g@mymail.nyp.edu.sg>
2021-08-17 12:56:12 +0800

    shawn

--- src/app/backworkouts/backworkouts.page.ts
+++ src/app/backworkouts/backworkouts.page.ts
@@ -1,5 +1,11 @@
-import { HttpClient } from '@angular/common/http';
-import { Component,} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NewsfeedsService } from '../shared/services/newsfeeds.service';
+import { NewsfeedsLikesService } from '../shared/services/newsfeedslikes.service';
+import { Storage } from '@capacitor/storage';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IonSearchbar, ToastController } from '@ionic/angular';
+import { Newsfeeds } from '../shared/model/Newsfeeds';
 import { WorkoutsService } from '../shared/services/workouts.service';
 
 @Component({
@@ -9,15 +15,20 @@ import { WorkoutsService } from '../shared/services/workouts.service';
 })
 export class BackworkoutsPage{
 
+    isShown : boolean = false;
+    id: string
+
   FitnessList: any = [];
 
-  constructor(public http: HttpClient, private workoutService: WorkoutsService,) {
+  constructor(public http: HttpClient, private workoutService: WorkoutsService, private Snaprouter:ActivatedRoute, public newsfeedService: NewsfeedsService,
+    public toastController: ToastController,public router: Router, public newsfeedlikesService : NewsfeedsLikesService) {
     this.FitnessList = this.workoutService.getProducts();
-    
+    this.id = this.Snaprouter.snapshot.params.id;
    }
 
   ngOnInit() {
     this.getBack();
+    this.loginUser()
   }
 
   async getBack(){
@@ -26,5 +37,25 @@ export class BackworkoutsPage{
     this.FitnessList=data
   })
 }
+
+checkadmin1(){
+  console.log(this.isShown)
+  console.log(this.id)
+  if (this.id == "7")
+  {
+    this.isShown = true
+    
+  }
+
+}
+
+  async loginUser() {
+    const { value } = await Storage.get({ key: 'userID' });
+    this.id = value;
+    console.log('tab5userid: ', this.id);
+    this.checkadmin1()
+
+  }
+
 
 }
